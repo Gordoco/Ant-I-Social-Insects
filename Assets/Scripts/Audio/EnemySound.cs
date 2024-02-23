2562c2ca21006869a102fe28404ae9f76a33fe4a
rdecena1 <rdecena@academic.rrc.ca>
2024-02-23 17:48:49 -0600

    created sfx system

---
+++ Assets/Scripts/Audio/EnemySound.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // hook onto these events for an enemy
+
+        // enemy spawn
+        // enemy dead
+        // enemy interaction
+
+        // interaction filters based on enum
+    }
+}
