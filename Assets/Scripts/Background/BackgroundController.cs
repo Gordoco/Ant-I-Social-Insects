c3d240e98edea5054909dedefaf7a481abfa8b3b
CameronJung <c.jung@mymts.net>
2024-02-23 19:36:16 -0600

    Scrolling Backgrounds Working
    - Background panels now move to the correct positions
    - Background panels can be rectangles
     - In order to work the background Controller must have the correct dimensions of the panels

--- Assets/Scripts/Background/BackgroundController.cs
+++ Assets/Scripts/Background/BackgroundController.cs
@@ -6,63 +6,61 @@ public class BackgroundController : MonoBehaviour
 {
 
     [SerializeField] private GameObject Player;
-    [SerializeField] private GameObject Stuff;
     [SerializeField] private Camera cam;
 
-    private Vector2Int cameraResolution;
-    [SerializeField] private Vector2Int panelResolution = new Vector2Int(1024,1024);
 
-    //The distance from one panel to the next
-    private float panelDistance = 16.0f;
+    //The dimensions of a background panel in world units
+    [SerializeField] private float panelHeight = 16.0f;
+    private float halfHeight;
+    [SerializeField] private float panelWidth = 32.0f;
+    private float halfWidth;
 
 
     //Odd index Panels start on the second row
     //higher number panels start thurther to the right
     [SerializeField] GameObject[] panels;
-    private Vector2 cameraDimensions;
 
-    private bool oddOntop = true;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        cameraDimensions = new Vector2(cam.pixelRect.width, cam.pixelRect.height) / panels[0].GetComponent<SpriteRenderer>().sprite.pixelsPerUnit;
-        Debug.Log(cameraDimensions);
-
-
-        Debug.Log("Upper edge: " + (panels[0].transform.position.y + panelDistance / 2));
-        Debug.Log("Camera bound: " + (cam.transform.position.y - cameraDimensions.y));
+        
+        halfWidth = panelWidth / 2;
+        halfHeight = panelHeight / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+
         Vector3 camPosition = cam.gameObject.transform.position;
 
-        GameObject bottomPanel = panels[0];
-        if (!oddOntop)
-        {
-            bottomPanel = panels[1];
-        }
+        Vector3Int gridCamPosition = new Vector3Int(Mathf.RoundToInt( camPosition.x / panelWidth), Mathf.RoundToInt(camPosition.y / panelHeight), Mathf.RoundToInt(camPosition.z));
 
-        //Moving bottom panels up
-        if (bottomPanel.transform.position.y + panelDistance/2 <= camPosition.y - cameraDimensions.y)
-        {
-            if (oddOntop)
-            {
-                panels[0].transform.position = new Vector3(panels[0].transform.position.x, panels[1].transform.position.y + panelDistance, panels[0].transform.position.z);
-                panels[2].transform.position = new Vector3(panels[2].transform.position.x, panels[3].transform.position.y + panelDistance, panels[2].transform.position.z);
-                oddOntop = false;
-            }
-            else
-            {
-                panels[1].transform.position = new Vector3(panels[1].transform.position.x, panels[0].transform.position.y + panelDistance, panels[1].transform.position.z);
-                panels[2].transform.position = new Vector3(panels[3].transform.position.x, panels[2].transform.position.y + panelDistance, panels[3].transform.position.z);
-                oddOntop = true;
-            }
-        }
+
+
+        int xflip = 1 - 2 * (Mathf.Abs(gridCamPosition.x) % 2);
+
+        int yflip = 1 - 2 * (Mathf.Abs(gridCamPosition.y) % 2);
+
+
+        panels[0].transform.position = new Vector3(gridCamPosition.x * panelWidth - (xflip * halfWidth), 
+            gridCamPosition.y * panelHeight - (yflip * halfHeight), panels[0].transform.position.z);
+
+        panels[1].transform.position = new Vector3(gridCamPosition.x * panelWidth - (xflip * halfWidth),
+            gridCamPosition.y * panelHeight + (yflip * halfHeight), panels[1].transform.position.z);
+
+        panels[2].transform.position = new Vector3(gridCamPosition.x * panelWidth + (xflip * halfWidth),
+            gridCamPosition.y * panelHeight - (yflip * halfHeight), panels[2].transform.position.z);
+
+        panels[3].transform.position = new Vector3(gridCamPosition.x * panelWidth + (xflip * halfWidth), 
+            gridCamPosition.y * panelHeight + (yflip * halfHeight), panels[3].transform.position.z);
+
+
+        
 
     }
 }
