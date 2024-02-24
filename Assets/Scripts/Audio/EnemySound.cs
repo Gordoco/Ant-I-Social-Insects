74102f0774fa49045a543a6f8887d3fda528ed3b
rdecena1 <rdecena@academic.rrc.ca>
2024-02-23 18:30:34 -0600

    added enemy sound to chaser bee

--- Assets/Scripts/Audio/EnemySound.cs
+++ Assets/Scripts/Audio/EnemySound.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemySound : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private BaseBee _baseEnemy;
+    [SerializeField] private SFXPlayer _sfxPlayer;
+
+    [Space(10)]
+
+    [SerializeField] private SFX _bounce;
+    [SerializeField] private SFX _damage;
+    [SerializeField] private SFX _kill;
+
+    [SerializeField] private SFX _spawned;
+    [SerializeField] private SFX _dead;
+
+    private void Awake()
     {
-        // hook onto these events for an enemy
+        _baseEnemy.Spawned += BaseEnemy_Spawned;
+        _baseEnemy.Dead += BaseEnemy_Dead;
+        _baseEnemy.Interaction += BaseEnemy_Interaction;
+    }
 
-        // enemy spawn
-        // enemy dead
-        // enemy interaction
+    private void BaseEnemy_Interaction(object sender, FInteraction e)
+    {
+        SFX sfx = null;
+        switch (e.result)
+        {
+            case EInteractionResult.Bounce:
+                sfx = _bounce;
+                break;
+            case EInteractionResult.Damage:
+                sfx = _damage;
+                break;
+            case EInteractionResult.Kill:
+                sfx = _kill;
+                break;
+        }
 
-        // interaction filters based on enum
+        if (sfx == null) return;
+        _sfxPlayer.PlaySFX(sfx);
+    }
+
+    private void BaseEnemy_Dead(object sender, System.EventArgs e)
+    {
+        if (_dead == null) return;
+        _sfxPlayer.PlaySFX(_dead);
+    }
+
+    private void BaseEnemy_Spawned(object sender, System.EventArgs e)
+    {
+        if (_spawned == null) return;
+        _sfxPlayer.PlaySFX(_spawned);
     }
 }
